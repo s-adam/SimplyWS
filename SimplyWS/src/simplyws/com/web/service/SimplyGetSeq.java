b94b8e37ce4b92038c585880b28acefbaca0f95c
Adam <stanekadam1@gmail.com>
2015-06-24 13:19:06 +0200

    Added some comments for doxygen documentation.

--- SimplyWS/src/simplyws/com/web/service/SimplyGetSeq.java
+++ SimplyWS/src/simplyws/com/web/service/SimplyGetSeq.java
@@ -1,15 +1,29 @@
 package simplyws.com.web.service;
 
+/**
+ * \brief Class SimplyGetSeq 
+ * \details Class SimplyGetSeq is a class that holds
+ * one function count sequences
+ **/
+
 public class SimplyGetSeq {
-	
-	public static int CountSequences(String zbior, String ciag) {
+	/**
+	 * \brief Function finding sequence in string of chars.
+	 * \details Searches the repeats of String ciag in String zbior, counts it and 
+	 * overwrite it to variable counts
+	 * \param zbior main base string which in where is searched
+	 * \param ciag is the search string in zbior
+	 * \return counts of ciag in zbior
+	 **/
+	public static int countSequences(String zbior, String ciag) {
 		// String zbior = "abcabcaabca";
 		// String ciag = "ab";
 		int count = 0, c = 0, i = 0;
 		while (i < zbior.length()) {
 
 			for (int j = 0; j < ciag.length(); j++) {
-				//System.out.println(i + ": " + j + ": " + c + ": " + count + ": ");
+				// System.out.println(i + ": " + j + ": " + c + ": " + count +
+				// ": ");
 
 				if (zbior.charAt(i) == ciag.charAt(j)) {
 					c++;
