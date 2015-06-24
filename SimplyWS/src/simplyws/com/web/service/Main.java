7ba7192ead12720693505214ffcbb61a63c689eb
Adam <stanekadam1@gmail.com>
2015-06-24 09:49:52 +0200

    Moved function

--- SimplyWS/src/simplyws/com/web/service/Main.java
+++ SimplyWS/src/simplyws/com/web/service/Main.java
@@ -2,43 +2,10 @@ package simplyws.com.web.service;
 
 public class Main {
 
-	public static int CountSequences(String zbior, String ciag) {
-		// String zbior = "abcabcaabca";
-		// String ciag = "ab";
-		int count = 0, c = 0, i = 0;
-		while (i < zbior.length()) {
-
-			for (int j = 0; j < ciag.length(); j++) {
-				//System.out.println(i + ": " + j + ": " + c + ": " + count + ": ");
-
-				if (zbior.charAt(i) == ciag.charAt(j)) {
-					c++;
-					i++;
-				} else if (c != 0) {
-					j = ciag.length();
-					c = 0;
-					// i++;
-				} else if (ciag.charAt(0) != zbior.charAt(i)) {
-					i++;
-				}
-				if (c == ciag.length()) {
-					count++;
-					c = 0;
-					j = ciag.length();// exithh
-				}
-				if (i == zbior.length())
-					break;
-
-			}
-
-		}
-		// System.out.println("HEHEHUE " + count);
-		return count;
-	}
 
 	public static void main(String[] args) {
 
-		System.out.println(CountSequences("abcabcaabca", "abca"));
+		System.out.println(SimplyGetSeq.CountSequences("abcabcaabca", "abca"));
 
 	}
 
--- SimplyWS/src/simplyws/com/web/service/SimplyGetSeq.java
+++ SimplyWS/src/simplyws/com/web/service/SimplyGetSeq.java
@@ -1,5 +1,38 @@
 package simplyws.com.web.service;
 
 public class SimplyGetSeq {
+	
+	public static int CountSequences(String zbior, String ciag) {
+		// String zbior = "abcabcaabca";
+		// String ciag = "ab";
+		int count = 0, c = 0, i = 0;
+		while (i < zbior.length()) {
+
+			for (int j = 0; j < ciag.length(); j++) {
+				//System.out.println(i + ": " + j + ": " + c + ": " + count + ": ");
+
+				if (zbior.charAt(i) == ciag.charAt(j)) {
+					c++;
+					i++;
+				} else if (c != 0) {
+					j = ciag.length();
+					c = 0;
+					// i++;
+				} else if (ciag.charAt(0) != zbior.charAt(i)) {
+					i++;
+				}
+				if (c == ciag.length()) {
+					count++;
+					c = 0;
+					j = ciag.length();// exithh
+				}
+				if (i == zbior.length())
+					break;
+
+			}
+
+		}
+		return count;
+	}
 
 }
